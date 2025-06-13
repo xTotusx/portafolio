586f4b6db1100874e3d1ed41cf57bf1374ec9de7
Andres <cepedatotus@gmail.com>
2025-06-12 20:04:12 -0600

    quitando el modo oscuro del navegador

--- script.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.createElement('button');
-toggleBtn.textContent = "Cambiar modo";
-document.body.prepend(toggleBtn);
-
-toggleBtn.addEventListener('click', () => {
-  document.body.classList.toggle('oscuro');
-});
